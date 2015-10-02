bf1c9f7e87e45d2ff3cebae841b1b51dcc4b62b2
unknown <naveed.ahmad@NAVEED.care.com>
2015-10-02 09:51:47 +0500

    Initial commit

---
+++ app/src/main/java/com/example/naveedahmad/dragsortdemo/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.naveedahmad.dragsortdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import com.mobeta.android.dslv.DragSortController;
+import com.mobeta.android.dslv.DragSortListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends Activity {
+
+    DragSortListView listView;
+    ArrayAdapter<String> adapter;
+
+    private DragSortListView.DropListener onDrop = new DragSortListView.DropListener() {
+        @Override
+        public void drop(int from, int to) {
+            if (from != to) {
+                String item = adapter.getItem(from);
+                adapter.remove(item);
+                adapter.insert(item, to);
+            }
+        }
+    };
+
+    private DragSortListView.RemoveListener onRemove = new DragSortListView.RemoveListener() {
+        @Override
+        public void remove(int which) {
+            adapter.remove(adapter.getItem(which));
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (DragSortListView) findViewById(R.id.listView);
+        String[] names = new String[]{"Pakistan", "India", "China", "UAE, USA", "UK", "Canada", "Australia", "Africa"};
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList(names));
+        adapter = new ArrayAdapter<String>(this,
+                R.layout.row_item, R.id.textView1, list);
+        listView.setAdapter(adapter);
+        listView.setDropListener(onDrop);
+        listView.setRemoveListener(onRemove);
+
+        DragSortController controller = new DragSortController(listView);
+        controller.setDragHandleId(R.id.imageView);
+        //controller.setClickRemoveId(R.id.);
+        controller.setRemoveEnabled(false);
+        controller.setSortEnabled(true);
+        controller.setDragInitMode(1);
+        //controller.setRemoveMode(removeMode);
+
+        listView.setFloatViewManager(controller);
+        listView.setOnTouchListener(controller);
+        listView.setDragEnabled(true);
+
+
+    }
+
+}
